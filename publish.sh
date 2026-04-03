b0f71550f5c57df9a0fa220f80e5fa2c1eac845a
Adam Twardoch <adam@twardoch.com>
2026-04-03 16:54:17 +0200

    chore: Build docs for release

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
 cd "$(dirname "$0")"
 python -m mkdocs build
+git add -A
+git diff --cached --quiet || git commit -m "chore: Build docs for release"
 uvx hatch clean
-uvx hatch build
 gitnextver
+uvx hatch build
 uv publish
